9ef9cfdc84a0b67a981a6f34ce15b0dad1b64b22
Edu58 <edumuriithi58@gmail.com>
2021-12-20 17:06:10 +0300

    added-random-recipe

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ const meal_item = document.querySelector("meal-item");
 const recipe = document.getElementById("recipe")
 const recipe_guide = document.getElementById("instructions")
 const recipe_close_btn = document.getElementById("close-btn")
+const search_txt = document.getElementById("search-results-txt")
+
+window.onload = fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+  .then((response) => response.json())
+  .then((data) => {
+    const randommeal = data.meals[0];
+    console.log(meal)
+    search_txt.textContent = "Random Meal";
+    html1 = `
+                <div class="meal-item" data-id="${randommeal.idMeal}">
+                    <strong>${randommeal.strMeal}</strong>
+                    <img src="${randommeal.strMealThumb}" alt="food image" />
+                    <button class="recipe-btn">Recipe</button>
+                </div>
+                  `;
+    meal.innerHTML = html1;
+    meal.classList.add("randommeal")
+  })
 
 search.addEventListener("click", getMeals);
 meal.addEventListener("click", showRecipe);
@@ -21,6 +39,7 @@ function getMeals() {
       let html = "";
 
       if (data.meals) {
+        search_txt.textContent = "search results:";
         data.meals.forEach((meal) => {
           html += `
                 <div class="meal-item" data-id="${meal.idMeal}">
@@ -31,6 +50,7 @@ function getMeals() {
                   `;
         });
         meal.classList.remove("notFound")
+        meal.classList.remove("randommeal");
       } else {
         html = "Sorry, we didn't find any food with the searched ingredient!";
         meal.classList.add("notFound")
